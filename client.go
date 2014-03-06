cb52a5c0b2f208958e39a2911930f61a698bce72
breedx <bri4n.r33d@gmail.com>
2014-03-06 18:13:15 +0000

    update

--- client.go
+++ client.go
@@ -47,16 +47,16 @@ func main() {
     service := "10.38.207.141:8000"
     serverAddr, err := net.ResolveTCPAddr("tcp4", service)
     if err != nil { log.Fatal(err) }
-    conn, err := net.DialTCP("tcp4", nil, serverAddr)
-    conn.SetNoDelay(true)
+    conn, err := net.DialTCP("tcp4", nil, serverAddr)    
     if err != nil { log.Fatal(err) }
+    
     //conn.Write("hello")
     writer := bufio.NewWriter(conn)
     m := make(map[string][]byte)    
       
     //buf := make([]byte, 500)
     startTime := time.Now()
-    for i := 0; i < 10; i++ {
+    for i := 0; i < 10000; i++ {
         m["i"] = []byte("i need a longer packet omg so what is umm.... this over here how long does it take")    
         buf := *pack(m)          
         writer.Write(buf)
